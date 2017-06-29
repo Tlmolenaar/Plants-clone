79bc1b341f525a9fd2f64a39c2bfa21f68e52181
Thomas Molenaar <tlmolenaar@hotmail.com>
2017-06-29 13:26:08 +0100

    created splash screen

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class LevelManager : MonoBehaviour {
+	
+	public float autoLoadNextLevelAfter = 5;
+	
+	void Start(){
+		Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+	
+	public void LoadLevel(string name){
+		Debug.Log ("Level load requested for: "+name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log ("Quit game");
+		Application.Quit ();
+	}
+	
+	public void LoadNextLevel (){
+		Application.LoadLevel(Application.loadedLevel +1);
+	}
+	
+	
+}
