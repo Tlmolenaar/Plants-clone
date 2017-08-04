48f68d7c70d5390662e7a5492006e5b9fe20009a
Thomas Molenaar <tlmolenaar@hotmail.com>
2017-08-04 17:21:29 +0100

    initiated playerprefs manager

---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefsManager : MonoBehaviour {
+	const string MASTER_VOLUME_KEY = "master_volume";
+	const string DIFFICULTY_KEY = "difficulty";
+	const string LEVEL_KEY = "level_unlocked_";
+	
+	public static void SetMasterVolume(float volume){
+		if(volume > 0f && volume < 1f){
+			PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+		}
+		else{
+			Debug.LogError("Master volume out of range");
+		}
+	}
+	
+	public static float GetMasterVolume(){
+		return PlayerPrefs.GetFloat (MASTER_VOLUME_KEY);
+	}
+	
+	public static void SetDifficuty(float diff){
+		if(diff >= 0f && diff <= 1f){
+			PlayerPrefs.SetFloat(DIFFICULTY_KEY, diff);
+		}
+		else{
+			Debug.LogError("Difficulty setting out of range");
+		}
+	}
+	
+	public static float GetDifficulty(){
+		return PlayerPrefs.GetFloat(DIFFICULTY_KEY);
+	}
+	
+	public static void UnlockLevel (int level){
+		if(level <= Application.levelCount -1){
+			PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(), 1);
+		}
+		else{
+			Debug.LogError("Trying to load level that doesn't exist");
+		}
+	}
+	
+	public static bool IsLevelUnlocked(int level){
+		int levelValue =  PlayerPrefs.GetInt(LEVEL_KEY + level.ToString()); 
+		bool isUnlocked = (levelValue == 1);
+		
+		if(level <= Application.levelCount -1){
+			return isUnlocked;
+		}
+		else{
+			return false;
+		}
+	
+	}
+}
